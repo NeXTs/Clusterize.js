11f771980bf326a05677e22c9b9dbb62b9fb48fc
Denis Lukov <denismassters@gmail.com>
2016-06-28 12:47:13 +0300

    Added Google Closure Compiler's externs file. Closes #68

---
+++ externs.js
@@ -0,0 +1,30 @@
+/**
+ * @fileoverview Closure Compiler externs for Clusterize.js 0.16.0.
+ * @see https://developers.google.com/closure/compiler/docs/api-tutorial3
+ * @externs
+ */
+
+var Clusterize = {
+    "scroll_elem": {},
+    "content_elem": {},
+    "tag": {},
+    "rows_in_block": {},
+    "blocks_in_cluster": {},
+    "show_no_data_row": {},
+    "no_data_text": {},
+    "no_data_class": {},
+    "keep_parity": {},
+    "callbacks": {
+        "clusterWillChange": function() {},
+        "clusterChanged": function() {},
+        "scrollingProgress": function() {}
+    },
+    "update": function() {},
+    "append": function() {},
+    "prepend": function() {},
+    "refresh": function() {},
+    "getRowsAmount": function() {},
+    "getScrollProgress": function() {},
+    "clear": function() {},
+    "destroy": function() {}
+}
